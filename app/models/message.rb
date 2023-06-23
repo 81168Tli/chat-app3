f9e0722daa5e3f1d0bef7f343ac1c16a799507af
81168Tli <135498013+81168Tli@users.noreply.github.com>
2023-06-23 11:14:46 +0900

    Revert "Merge pull request #10 from 81168Tli/メッセージ投稿機能の実装"
    This reverts commit 675a6551063643b8bc1a0213a90de5a614666013, reversing
    changes made to 430053ab08f6f5aa074d10b265184d8952f848d2.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,2 @@
 class Message < ApplicationRecord
-  belongs_to :room
-  belongs_to :user
-
-  validates :content, presence: true
-
-
 end
--- db/migrate/20230619082239_addcontent_to_messages.rb
+++
@@ -1,4 +0,0 @@
-class AddcontentToMessages < ActiveRecord::Migration[7.0]
-  def change
-  end
-end
